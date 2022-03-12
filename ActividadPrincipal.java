6dab63b61db495c7ee41616f758096844f2d1cdb
Julio Fabian de la Rosa Izquierdo <Fabian87_delarosa@outlook.com>
2022-03-12 15:15:31 -0600

    se agregaron prints a la act principal.

--- ActividadPrincipal.java
+++ ActividadPrincipal.java
@@ -2,5 +2,8 @@ public class ActividadPrincipal {
 
     public static void main(String []args) {
         
+        System.out.println("Hola Mundo! 1");
+        System.out.println("Hola Mundo! 2");
+        System.out.println("Hola Mundo! 3");
     }
 }
